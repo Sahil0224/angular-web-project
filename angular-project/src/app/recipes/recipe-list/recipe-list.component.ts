f9920d4ce3a4d8e68f4a9548a45e6e1c35894182
Patel <sahil.patel@fiserv.com>
2024-01-24 16:19:18 -0500

    Able to display recipe-details.

--- angular-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit{
   recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.primaverakitchen.com/wp-content/uploads/2018/05/Whole30-Bunless-Turkey-Burger-Recipe-Primavera-Kitchen-1.jpg'),
     new Recipe('A Test Recipe', 'This is simply a test', 'https://www.primaverakitchen.com/wp-content/uploads/2018/05/Whole30-Bunless-Turkey-Burger-Recipe-Primavera-Kitchen-1.jpg')
+
   ];
   
   constructor() {}
