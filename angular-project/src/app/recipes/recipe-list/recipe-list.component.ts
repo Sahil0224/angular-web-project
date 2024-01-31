5194b36a38c025b37bfd478b110fa1a3a7169f73
Patel <sahil.patel@fiserv.com>
2024-01-31 15:20:10 -0500

    Section6.

--- angular-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,9 +7,10 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent implements OnInit{
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://www.primaverakitchen.com/wp-content/uploads/2018/05/Whole30-Bunless-Turkey-Burger-Recipe-Primavera-Kitchen-1.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.primaverakitchen.com/wp-content/uploads/2018/05/Whole30-Bunless-Turkey-Burger-Recipe-Primavera-Kitchen-1.jpg')
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://www.primaverakitchen.com/wp-content/uploads/2018/05/Whole30-Bunless-Turkey-Burger-Recipe-Primavera-Kitchen-1.jpg')
 
   ];
   
@@ -17,5 +18,9 @@ export class RecipeListComponent implements OnInit{
 
   ngOnInit(): void {
       
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
   }
 }
