e3d6fde2dcf5e42d862d1a0943086c0feb351177
Patel <sahil.patel@fiserv.com>
2024-02-08 16:11:32 -0500

    Able to use services, add ingredients to shopping list.

--- angular-project/src/app/recipes/recipe.service.ts
+++ angular-project/src/app/recipes/recipe.service.ts
@@ -1,16 +1,29 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://www.primaverakitchen.com/wp-content/uploads/2018/05/Whole30-Bunless-Turkey-Burger-Recipe-Primavera-Kitchen-1.jpg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://www.primaverakitchen.com/wp-content/uploads/2018/05/Whole30-Bunless-Turkey-Burger-Recipe-Primavera-Kitchen-1.jpg')
+        new Recipe('A Burger', 'This is the best burger.', 'https://tse1.mm.bing.net/th/id/OIP.aJflLmSm3dGKvwHdqvuBYwAAAA?w=202&h=158&c=7&r=0&o=5&pid=1.7', [
+            new Ingredient('Meat', 1),
+            new Ingredient('Onion', 1)
+        ]),
+        new Recipe('Salad', 'This is simply a healthy Salad.', 'https://tse4.mm.bing.net/th/id/OIP.nwknMlMuvlf9WfKjsueLQwHaE8?w=208&h=180&c=7&r=0&o=5&pid=1.7', [
+            new Ingredient('Corn', 1),
+            new Ingredient('Cheese', 1)
+        ])
     
       ];
-
+    constructor(private slService: ShoppingListService) {}
     getRecipies() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients)
     }
 }
