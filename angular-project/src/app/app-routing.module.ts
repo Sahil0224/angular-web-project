65aab4ab15f4976f3406e3590cf3e99b7ff538ff
Patel <sahil.patel@fiserv.com>
2024-03-08 17:13:58 -0500

    Optimizing the modules.

--- angular-project/src/app/app-routing.module.ts
+++ angular-project/src/app/app-routing.module.ts
@@ -1,23 +1,9 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { RecipesComponent } from "./recipes/recipes.component";
-import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
-import { RecipesResolverService } from "./recipes/recipes-resolver.service";
 import { AuthComponent } from "./auth/auth.component";
-import { AuthGaurd } from "./auth/auth.guard";
 
 const appRoutes: Routes = [
     {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-    {path: 'recipes', component: RecipesComponent, children: [
-        {path: '', component: RecipeStartComponent, canActivate: [AuthGaurd]},
-        {path: 'new', component: RecipeEditComponent},
-        {path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService]},
-        {path: ':id/edit', component: RecipeEditComponent,  resolve: [RecipesResolverService]}
-    ]},
-    {path: 'shopping-list', component: ShoppingListComponent},
     {path: 'auth', component: AuthComponent}
 ]
 @NgModule({
